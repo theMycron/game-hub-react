7b89808ab21a752c176d39f738f5687f46e95d39
theMycron <134844972+theMycron@users.noreply.github.com>
2024-06-09 16:51:38 +0300

    completed Material UI import and added test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Box from "@mui/material/Box";
+import { Button, Typography } from "@mui/material";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box sx={{ width: "100%", maxWidth: 800 }}>
+        <Typography variant="h1" gutterBottom>
+          Game Hub Website
+        </Typography>
+        <Button variant="outlined">Click ME!</Button>
+      </Box>
+    </>
+  );
 }
 
 export default App;
