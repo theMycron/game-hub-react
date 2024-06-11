524f3fd8bbd3c1972b87a93b84f0872506adb152
theMycron <134844972+theMycron@users.noreply.github.com>
2024-06-11 14:20:45 +0300

    added genre list hook

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,16 +1,19 @@
 import Box from "@mui/material/Box";
 import { Button, Stack, Typography } from "@mui/material";
+import useGenres from "../../hooks/useGenres.ts";
 
 function SideBar() {
+  const { genres } = useGenres();
+
   return (
     <Box>
       <Stack spacing={1} alignItems="start" padding={2}>
         <Typography variant="h5" fontWeight={"bold"}>
           Genres
         </Typography>
-        <Button>Action</Button>
-        <Button>Adventure</Button>
-        <Button>Puzzle</Button>
+        {genres.map((genre) => (
+          <Button key={genre.id}>{genre.name}</Button>
+        ))}
       </Stack>
     </Box>
   );
