e8c7e3e29288b35cdff66015a3970087cd19bd45
theMycron <134844972+theMycron@users.noreply.github.com>
2024-06-13 13:59:01 +0300

    added CORS header (hope it works)

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
+  withCredentials: false,
+  headers: {
+    post: {
+      "Access-Control-Allow-Origin": true,
+    },
+  },
   params: {
     key: "placeholder",
   },
