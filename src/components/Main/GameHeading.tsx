4376281e4b9954aa868b9240000bdbea1f4c16e1
theMycron <134844972+theMycron@users.noreply.github.com>
2024-06-22 12:08:28 +0300

    fixed game heading, now searches for platform and genre using ID instead of accessing them from gameQuery

--- src/components/Main/GameHeading.tsx
+++ src/components/Main/GameHeading.tsx
@@ -1,12 +1,23 @@
 import { Typography } from "@mui/material";
 import { GameQuery } from "../../App.tsx";
+import { platformsData } from "../../data/platforms.ts";
+import genres from "../../data/genres.ts";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 function GameHeading({ gameQuery }: Props) {
-  const heading = `${gameQuery.platform?.name || ""} ${gameQuery.genre?.name || ""} Games`;
+  // get platform and genre using ID
+  const platform = platformsData.results.find(
+    (platform) => platform.id === gameQuery.platformId,
+  )?.name;
+
+  const genre = genres.results.find(
+    (genre) => genre.id === gameQuery.genreId,
+  )?.name;
+
+  const heading = `${platform ? platform : ""} ${genre ? genre : ""} Games`;
 
   return (
     <Typography variant="h2" fontWeight={"bold"}>
